17321b137cc712f024417b6ff0d32298af5a80d3
jitspoe <jitspoe@yahoo.com>
2023-09-04 19:48:57 -0400

    Ported console to 4.x.

--- addons/console/Console.gd
+++ addons/console/Console.gd
@@ -7,16 +7,16 @@ signal console_unknown_command
 
 
 class ConsoleCommand:
-	var function
-	var param_count
+	var function : Callable
+	var param_count : int
 	func _init(in_function, in_param_count):
 		function = in_function
 		param_count = in_param_count
 
 
-onready var control := Control.new()
-onready var rich_label := RichTextLabel.new()
-onready var line_edit := LineEdit.new()
+@onready var control := Control.new()
+@onready var rich_label := RichTextLabel.new()
+@onready var line_edit := LineEdit.new()
 
 var console_commands := {}
 var console_history := []
@@ -33,44 +33,44 @@ func _ready():
 	rich_label.scroll_following = true
 	rich_label.anchor_right = 1.0
 	rich_label.anchor_bottom = 0.5
-	rich_label.add_stylebox_override("normal", load("res://addons/console/console_background.tres"))
+	rich_label.add_theme_stylebox_override("normal", load("res://addons/console/console_background.tres"))
 	control.add_child(rich_label)
 	rich_label.text = "Development console.\n"
 	line_edit.anchor_top = 0.5
 	line_edit.anchor_right = 1.0
 	line_edit.anchor_bottom = 0.5
 	control.add_child(line_edit)
-	line_edit.connect("text_entered", self, "on_text_entered")
+	line_edit.text_submitted.connect(on_text_entered)
 	control.visible = false
-	pause_mode = PAUSE_MODE_PROCESS
-	add_command("quit", self, "quit", 0)
-	add_command("exit", self, "quit", 0)
+	process_mode = PROCESS_MODE_ALWAYS
+	add_command("quit", quit, 0)
+	add_command("exit", quit, 0)
 
 
 func _input(event : InputEvent):
 	if (event is InputEventKey):
-		if (event.physical_scancode == 96): # Reverse-nice.  Also ~ key.
+		if (event.physical_keycode == 96): # Reverse-nice.  Also ~ key.
 			if (event.pressed):
 				toggle_console()
-			get_tree().set_input_as_handled()
-		elif (event.physical_scancode == KEY_ESCAPE && control.visible): # Disable console on ESC
+			get_viewport().set_input_as_handled()
+		elif (event.physical_keycode == KEY_ESCAPE && control.visible): # Disable console on ESC
 			if (event.pressed):
 				toggle_console()
-				get_tree().set_input_as_handled()
+				get_viewport().set_input_as_handled()
 	if (control.visible):
 		if (Input.is_action_just_pressed("ui_up")):
-			get_tree().set_input_as_handled()
+			get_viewport().set_input_as_handled()
 			if (console_history_index > 0):
 				console_history_index -= 1
 				if (console_history_index >= 0):
 					line_edit.text = console_history[console_history_index]
-					line_edit.caret_position = line_edit.text.length()
+					line_edit.caret_column = line_edit.text.length()
 		if (Input.is_action_just_pressed("ui_down")):
 			if (console_history_index < console_history.size()):
 				console_history_index += 1
 				if (console_history_index < console_history.size()):
 					line_edit.text = console_history[console_history_index]
-					line_edit.caret_position = line_edit.text.length()
+					line_edit.caret_column = line_edit.text.length()
 				else:
 					line_edit.text = ""
 
@@ -105,13 +105,13 @@ func on_text_entered(text : String):
 			var command_entry : ConsoleCommand = console_commands[command_string]
 			match command_entry.param_count:
 				0:
-					command_entry.function.call_func()
+					command_entry.function.call()
 				1:
-					command_entry.function.call_func(split_text[1] if split_text.size() > 1 else "")
+					command_entry.function.call(split_text[1] if split_text.size() > 1 else "")
 				2:
-					command_entry.function.call_func(split_text[1] if split_text.size() > 1 else "", split_text[2] if split_text.size() > 2 else "")
+					command_entry.function.call(split_text[1] if split_text.size() > 1 else "", split_text[2] if split_text.size() > 2 else "")
 				3:
-					command_entry.function.call_func(split_text[1] if split_text.size() > 1 else "", split_text[2] if split_text.size() > 2 else "", split_text[3] if split_text.size() > 3 else "")
+					command_entry.function.call(split_text[1] if split_text.size() > 1 else "", split_text[2] if split_text.size() > 2 else "", split_text[3] if split_text.size() > 3 else "")
 				_:
 					print_line("Commands with more than 3 parameters not supported.")
 		else:
@@ -119,8 +119,8 @@ func on_text_entered(text : String):
 			print_line("Command not found.")
 
 
-func add_command(command_name : String, object : Object, function_name : String, param_count : int = 0):
-	console_commands[command_name] = ConsoleCommand.new(funcref(object, function_name), param_count)
+func add_command(command_name : String, function : Callable, param_count : int = 0):
+	console_commands[command_name] = ConsoleCommand.new(function, param_count)
 
 
 func remove_command(command_name : String):
@@ -138,23 +138,21 @@ func add_input_history(text : String):
 
 
 func _enter_tree():
-	var console_history_file := File.new()
-	if (console_history_file.open("user://console_history.txt", File.READ) == OK):
+	var console_history_file := FileAccess.open("user://console_history.txt", FileAccess.READ)
+	if (console_history_file):
 		while (!console_history_file.eof_reached()):
 			var line := console_history_file.get_line()
 			if (line.length()):
 				add_input_history(line)
-		console_history_file.close()
 
 
 func _exit_tree():
-	var console_history_file := File.new()
-	if (console_history_file.open("user://console_history.txt", File.WRITE) == OK):
+	var console_history_file := FileAccess.open("user://console_history.txt", FileAccess.WRITE)
+	if (console_history_file):
 		var write_index := 0
 		var start_write_index := console_history.size() - 100 # Max lines to write
 		for line in console_history:
 			if (write_index >= start_write_index):
 				console_history_file.store_line(line)
 			write_index += 1
-		console_history_file.close()
 
--- addons/console/console_plugin.gd
+++ addons/console/console_plugin.gd
@@ -1,11 +1,7 @@
-tool
+@tool
 extends EditorPlugin
 
 
 func _enter_tree():
-	print ("Console plugin activated.")
+	print("Console plugin activated.")
 	add_autoload_singleton("Console", "res://addons/console/Console.gd")
-
-
-func _exit_tree():
-	print ("Console plugin exited.")
